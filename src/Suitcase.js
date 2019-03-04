7c77630812b43a0e9e5daabc70bf9be7b37e39bb
Gus <gusrichardson1@gmail.com>
2019-03-03 20:46:02 -0500

    added suitcase and list

--- src/Suitcase.js
+++ src/Suitcase.js
@@ -32,14 +32,18 @@ class Suitcase extends Component {
         return (
             <div className="clothingItems">
                 <div className="clothingForm">
+                    <label for="clothingInput">Add clothing here</label>
                     <input
                         onChange={(e) => this.changeUserInput(e.target.value)}
                         value={this.state.userInput}
                         type="text"
+                        id="clothingInput"
+                        placeholder="insert clothing"
                     />
-                    <button onClick={() => this.addClothing(this.state.userInput)}>Update Clothing</button>
+                    <button onClick={() => this.addClothing(this.state.userInput)}>Add to List</button>
                 </div>
                 <div className="suitcaseList">
+                    <h2>Clothing to Bring</h2>
                     <ul>
                         {this.state.clothingList.map((item) => {
                             return <li>{item}</li>
