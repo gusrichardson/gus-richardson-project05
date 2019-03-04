3ec74393c706289e96649ee1e52230d0d59d8008
Gus <gusrichardson1@gmail.com>
2019-03-04 13:13:28 -0500

    Updates

---
+++ precache-manifest.986389f95b5efeb2b2461bfcfc88b77b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d7e3ee41b2fbe4a89ac09f2bc030228",
+    "url": "/gusRichardsonProject05/static/media/cloudyDay.4d7e3ee4.jpg"
+  },
+  {
+    "revision": "9c36b7b6b9e694815cd5183561be0649",
+    "url": "/gusRichardsonProject05/static/media/brownSuitCase.9c36b7b6.png"
+  },
+  {
+    "revision": "6cf071899da16a5a720b",
+    "url": "/gusRichardsonProject05/static/js/runtime~main.6cf07189.js"
+  },
+  {
+    "revision": "ad8a8ee0c4da3bc2baed",
+    "url": "/gusRichardsonProject05/static/js/main.ad8a8ee0.chunk.js"
+  },
+  {
+    "revision": "7a7a449ad851c3fe329e",
+    "url": "/gusRichardsonProject05/static/js/2.7a7a449a.chunk.js"
+  },
+  {
+    "revision": "ad8a8ee0c4da3bc2baed",
+    "url": "/gusRichardsonProject05/static/css/main.b7c426b2.chunk.css"
+  },
+  {
+    "revision": "550362fe5a377342d403d70830d0fb97",
+    "url": "/gusRichardsonProject05/index.html"
+  }
+];
